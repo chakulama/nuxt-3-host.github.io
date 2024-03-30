cefa5fd9940eb31949cbb7bf071e7e4b9fd0e946
binod lama <binodlama@binods-MacBook-Pro.local>
2024-03-30 02:13:23 +0100

    neew changes

--- _nuxt/index.2Sko2oAK.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./entry.CAutscix.js";import"./swiper-vue.C59EKm1J.js";const r={};function t(c,n){return"about page here"}const a=e(r,[["render",t]]);export{a as default};
---
+++ _nuxt/index.hIL4VoHv.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./entry.D56T1jOL.js";import"./swiper-vue.C59EKm1J.js";const r={};function t(c,n){return"about page here"}const a=e(r,[["render",t]]);export{a as default};
